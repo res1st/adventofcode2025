caac195c0971b245a099185185d0a594614e18cc
Ingo Siebert <ingo_siebert@yahoo.de>
2025-12-29 18:16:20 +0100

    adds day3 part 2 solution

--- day3_power.py
+++ day3_power.py
@@ -23,20 +23,74 @@ def calulateMaxOfLine(line, startIndex, endIndex):
         # print(f"i={i}  currentValue={currentValue}  maxValue={maxValue}")
     return {"max": str(maxValue), "index": maxIndex}
 
-sum = 0
-for line in linesFile:
-    line = line.strip()
-    totalPower = 0
-    length = len(line)
-    print(f"line {line}")
+def part1():
+    sum = 0
+    for line in linesFile:
+        line = line.strip()
+        length = len(line)
+        print(f"line {line}")
 
-    max = calulateMaxOfLine(line, 0, length-1)
-    max2nd = calulateMaxOfLine(line, max["index"] + 1, length)
+        max = calulateMaxOfLine(line, 0, length-1)
+        max2nd = calulateMaxOfLine(line, max["index"] + 1, length)
 
-    intValue = int(max["max"] + max2nd["max"])
-    print(f"Max value is {intValue}")
-    sum += intValue
+        intValue = int(max["max"] + max2nd["max"])
+        print(f"Max value is {intValue}")
+        sum += intValue
+        
+        
+    print(f"Total Power Consumption: {sum}")
+
+# doesn't produce the biggest possible number
+# it removes the smallest digit regardless of position, which is wrong
+def removeLowestUntil12Digits(line):
+    while len(line) > 12:
+        minValue = 10
+        minIndex = -1
+        for i in range(len(line)):
+            currentValue = int(line[i])
+            if currentValue < minValue:
+                minValue = currentValue
+                minIndex = i
+        line = line[:minIndex] + line[minIndex+1:]
+    return line
+
+# monotonic stack
+# removes the previous digit if the current digit is larger
+def removeLowestUntil12DigitsWithStack(line):
+    targetLength = 12
+    numberOfDigitsToRemove = len(line) - targetLength
+    stack = []
+
+    for digit in line:
+        # While we still have removals left AND the stack isn't empty 
+        # AND the current digit is bigger than the last one added...
+        while numberOfDigitsToRemove > 0 and stack and stack[-1] < digit:
+            stack.pop()
+            numberOfDigitsToRemove -= 1
+        stack.append(digit)
     
-    
-print(f"Total Power Consumption: {sum}")
+    # If we still need to remove digits (e.g., input was "999999999999999"),
+    # remove them from the end.
+    result = "".join(stack[:targetLength])
+    return result
 
+def part2():
+    sum = 0
+    for line in linesFile:
+        line = line.strip()
+        length = len(line)
+        print(f"line {line}")
+
+        firstMax = calulateMaxOfLine(line, 0, length-11)
+        print(f"First max value is {firstMax['max']} at index {firstMax['index']}")
+        totalMaxValue = line[firstMax["index"]:]
+        print(f"Total max value is {totalMaxValue}")
+        max12DigitsValue = removeLowestUntil12DigitsWithStack(totalMaxValue)
+        print(f"12 digits value is {max12DigitsValue}")
+        
+        print("----")
+        sum += int(max12DigitsValue)
+        
+    print(f"Total Power Consumption (Part 2): {sum}")
+
+part2()
